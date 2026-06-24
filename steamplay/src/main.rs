728924a2bab8c1aa653db6ea8f9b3687c1fe56d9
stiles <stiles@grocks.org>
2026-06-24 01:19:04 -0500

    created function that will search for the steamapps folder given a path, still need to make it iterate into the subdirectories

--- steamplay/src/main.rs
+++ steamplay/src/main.rs
@@ -2,8 +2,55 @@ use std::fs;
 
 fn main() {
     println!("steamplay - 0.0.1");
-    let drives = enumerate_drives();
-    println!("valid drives : {:?}", drives);
+    enumerate_steam_library();
+}
+
+fn enumerate_steam_library() {
+    
+    // enumerate drives on a windows system
+    let valid_drives = enumerate_drives();
+
+    // start searching through each drive
+    let mut search_results = Vec::new();
+    for drive in valid_drives {
+        let paths = steam_library_search(&drive);
+        match paths {
+            Some(p) => {
+                search_results.push(p)
+            },
+            None => continue
+        }
+    }
+}
+
+fn steam_library_search(root: &String) -> Option<Vec<String>>  {
+
+    // search into sub-directories looking for the steamapps folder.
+    
+    let mut found_paths = Vec::new();
+
+    let dir_entries = match fs::read_dir(root) {
+        Ok(entries) => entries,
+        Err(_) => return None
+    };
+
+    for entry in dir_entries {
+        let entry = match entry {
+            Ok(s) => s,
+            Err(_) => return None
+        };
+
+        let entrystr = match entry.file_name().into_string() {
+            Ok(s) => s,
+            Err(_) => return None
+        };
+
+        if entrystr == "steamapps" {
+            found_paths.push(String::from(&entrystr));
+        }
+    }
+
+    Some(found_paths)
 }
 
 fn enumerate_drives() -> Vec<String> {
