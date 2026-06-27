c0e32963b858ba56aabe6630434b7f16f786104a
stiles <stiles@grocks.org>
2026-06-26 21:13:19 -0500

    created func find_games(), just searches through the steam library for executables. need to add a blacklist to filter certain executables. maybe investigate how to identify certain games by engine since they seem to follow a similar pattern, unsure on how reliable that would be

--- steamplay/src/main.rs
+++ steamplay/src/main.rs
@@ -1,16 +1,23 @@
 mod util;
 
+use std::path::PathBuf;
+
 fn main() {
 
     println!("steamplay - 0.0.1");
-    
+    let mut games = Vec::new();
     for path in find_steamapps() {
-        println!("{:?}", path);
+        for _games in find_games(path) {
+            games.push(_games);
+        }
     }
-    
+
+    for g in games {
+        println!("{:?}", g);
+    }
 }
 
-fn find_steamapps() -> Vec<std::path::PathBuf> {
+fn find_steamapps() -> Vec<PathBuf> {
 
     // Find all steamapps folder on the system
     let mut found_paths = Vec::new();
@@ -32,4 +39,27 @@ fn find_steamapps() -> Vec<std::path::PathBuf> {
         }
     }
     steamapp_paths
+}
+
+fn find_games(steamapps_path: PathBuf) -> Vec<PathBuf> {
+
+    // returns the paths to games found in steamapps.
+    //
+    // there is no standard packaging method, different games, different engines
+    // have different patterns on how they pack it. so we walk the directories,
+    // search for any executables and for now filter the trash exes from a blacklist
+
+    let mut path_executables = Vec::new();
+    let path_collection = util::fs::enumerate_directory_depth(steamapps_path, 5);
+
+    // filter for files that are executables based on file extension
+    for p in path_collection {
+        if let Some(ext) = p.extension() {
+            if ext == "exe" {
+                path_executables.push(p);
+            }
+        }
+    }
+    
+    path_executables
 }
---
+++ steamplay/src/util/fs.rs
@@ -0,0 +1,98 @@
+use std::path::PathBuf;
+use std::fs;
+
+pub fn enumerate_drives() -> Vec<PathBuf> {
+
+    // return a vector of valid paths that represent logical windows drives
+    let mut valid_paths = Vec::new();
+
+    // generate strings like typical windows disk drives
+    // a:/ b:/ c:/ etc...
+    let drive_strings: Vec<String> = ('a'..='z')
+        .map(|c| format!("{}:/", c))
+        .collect();
+
+    // turn strings into path, use 'exists' to check if valid
+    for s in drive_strings {
+        let path = PathBuf::from(&s);
+        if path.exists() {
+            valid_paths.push(path);
+        }
+    }
+
+    valid_paths
+}
+
+pub fn enumerate_directory(p: PathBuf) -> Vec<PathBuf> {
+
+    // reads a directory and returns the path of the content as a PathBuf vector
+    let mut directory_contents = Vec::new();
+    if let Ok(reader) = fs::read_dir(p) {
+        for entry in reader.flatten() {
+            directory_contents.push(entry.path());
+        }
+    }
+
+    directory_contents
+}
+
+fn path_filter_directories<'a>(path: &'a Vec<PathBuf>) -> Vec<PathBuf> {
+
+    // filter a PathBuf vector for directories only and return as vector
+    let mut found_directories = Vec::new();
+    for p in path {
+        if p.is_dir() {
+            found_directories.push(p.clone());
+        }
+    }
+    found_directories
+}
+
+pub fn enumerate_directory_depth(p: PathBuf, max_depth: u32) -> Vec<PathBuf> {
+    
+    // reads the path at "p", then based on "max_depth" will dive into the sub
+    // directories. after enumerating all paths down to the depth, return
+    // all paths found in a vector
+
+    let mut found_paths = enumerate_directory(p);
+    
+    // 0 depth is just returning the contents of the directory given
+    if 0 == max_depth {
+        return found_paths;
+    }
+
+    // check for directories in contents and add to search_queue
+    let mut search_queue = path_filter_directories(&found_paths);
+
+    for current_depth in 0..max_depth {
+
+        let mut new_paths = Vec::new();
+
+        if current_depth == max_depth { 
+            break; 
+        }
+        
+        loop {
+            // search queue till empty
+            if let Some(current_dir) = search_queue.pop() {
+                    new_paths.extend(
+                        enumerate_directory(current_dir)
+                    );
+            } else {
+                break;
+            }
+        }
+        // save the paths we found after emptying a queue
+        found_paths.extend(
+            new_paths.clone()
+        );
+
+        // filter the new paths for directories to add back into
+        // search queue, which is diving into the sub directory.
+        search_queue.extend(
+            path_filter_directories(&new_paths)
+        );
+    }
+
+    found_paths
+}
