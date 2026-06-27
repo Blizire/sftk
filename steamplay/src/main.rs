92504a86fcef982f2110b0abc5a40bc6f75787ec
stiles <stiles@grocks.org>
2026-06-26 19:10:05 -0500

    refactored the whole code base, made things way to complicated, settled on using PathBuf as a base type for the search operations which was a way better choice for interoperability . The code is cleaner and more readable while fully achieving the effect I wanted in the first place. Also note that the depth search could have been done recursively so we could reduce lines of code, but as a exercise I wanted to implement it without recursion

--- steamplay/src/main.rs
+++ steamplay/src/main.rs
@@ -1,61 +1,115 @@
+use std::path::PathBuf;
 use std::fs;
-//use std::path;
 
 fn main() {
+
     println!("steamplay - 0.0.1");
+    
+    let mut found_paths = Vec::new();
 
-    // Iterators for each drive on a windows system.
-    let drive_iterators = enumerate_drives();
-
-    for drive in drive_iterators {
-        enumerate_directories(drive);
+    // search through all windows drives, and sub-directories and return
+    // all paths enumerated N levels deep.
+    for root in enumerate_drives() {
+        found_paths.extend(
+            enumerate_directory_depth(root, 2)
+        );
     }
+
+    println!("{:?}", found_paths);
 }
 
-fn enumerate_directories(dir: std::fs::ReadDir) {
-    // assumes its being given an iterator from fs::read_dir, then will use that
-    // to enumerate the directories and sub-directories based on the "depth"
-    let mut search_paths = Vec::new();
-    // flatten is cool, silently drops errors and returns only valid values
-    for entry in dir.flatten() {
-        match entry.file_type() {
-            Ok(e) => {
-                if e.is_dir() {
-                    search_paths.push(entry);
-                }
-            },
-            Err(_) => {}
-        };
-    }
-    // TODO: implement depth somehow so we can dive into these sub directories
-    println!("{:?}", search_paths);
-}
+fn enumerate_drives() -> Vec<PathBuf> {
 
-fn enumerate_drives() -> Vec<std::fs::ReadDir> {
+    // return a vector of valid paths that represent logical windows drives
+    let mut valid_paths = Vec::new();
 
-    // searches for all valid windows drives, returns an iterator that
-    // can read through the directory listing.
-
-    let mut valid_drives = Vec::new();
-
-    // generate a vector of strings that represents typical windows disk drives
+    // generate strings like typical windows disk drives
     // a:/ b:/ c:/ etc...
-    let logical_drives: Vec<String> = ('a'..='z')
+    let drive_strings: Vec<String> = ('a'..='z')
         .map(|c| format!("{}:/", c))
         .collect();
 
-    for drive in logical_drives {
-        match fs::read_dir(&drive) {
-            Ok(e) => {
-                // pushes an iterator that can read the contents of these directories
-                // into a vector for return.
-                valid_drives.push(e);
-            }
-            Err(_) => {
-                continue
-            }
+    // turn strings into path, use 'exists' to check if valid
+    for s in drive_strings {
+        let path = PathBuf::from(&s);
+        if path.exists() {
+            valid_paths.push(path);
         }
     }
 
-    valid_drives
+    valid_paths
+}
+
+fn enumerate_directory(p: PathBuf) -> Vec<PathBuf> {
+
+    // reads a directory and returns the path of the content as a PathBuf vector
+    let mut directory_contents = Vec::new();
+    if let Ok(reader) = fs::read_dir(p) {
+        for entry in reader.flatten() {
+            directory_contents.push(entry.path());
+        }
+    }
+
+    directory_contents
+}
+
+fn path_filter_directories<'a>(path: &'a Vec<PathBuf>) -> Vec<PathBuf> {
+
+    // filter a PathBuf vector for directories only and return as vector
+    let mut found_directories = Vec::new();
+    for p in path {
+        if p.is_dir() {
+            found_directories.push(p.clone());
+        }
+    }
+    found_directories
+}
+
+fn enumerate_directory_depth(p: PathBuf, max_depth: u32) -> Vec<PathBuf> {
+    
+    // reads the path at "p", then based on "max_depth" will dive into the sub
+    // directories. after enumerating all paths down to the depth, return
+    // all paths found in a vector
+
+    let mut found_paths = enumerate_directory(p);
+    
+    // 0 depth is just returning the contents of the directory given
+    if 0 == max_depth {
+        return found_paths;
+    }
+
+    // check for directories in contents and add to search_queue
+    let mut search_queue = path_filter_directories(&found_paths);
+
+    for current_depth in 0..max_depth {
+
+        let mut new_paths = Vec::new();
+
+        if current_depth == max_depth { 
+            break; 
+        }
+        
+        loop {
+            // search queue till empty
+            if let Some(current_dir) = search_queue.pop() {
+                    new_paths.extend(
+                        enumerate_directory(current_dir)
+                    );
+            } else {
+                break;
+            }
+        }
+        // save the paths we found after emptying a queue
+        found_paths.extend(
+            new_paths.clone()
+        );
+
+        // filter the new paths for directories to add back into
+        // search queue, which is diving into the sub directory.
+        search_queue.extend(
+            path_filter_directories(&new_paths)
+        );
+    }
+
+    found_paths
 }
